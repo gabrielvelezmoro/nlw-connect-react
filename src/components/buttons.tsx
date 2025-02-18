4b7fc2e6d696baf3ec059c2c2d9cac279a6f39e8
Gabriel Velezmoro <gabrieuvelezmoro@gmail.com>
2025-02-18 10:03:52 -0300

    refactor: button props

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,11 +1,16 @@
-interface ButtonProps {
-    children: React.ReactNode;
-}
+import { ComponentProps, ReactNode } from "react";
+
+interface ButtonProps extends ComponentProps<"button"> { 
+    children: ReactNode;
+} 
+
 
 export function Button(props: ButtonProps) {
     return (
-        <button className="flex justify-between itens-center px-5 h=12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300">
-            {props.children }
-        </button>
+        <button 
+        className="flex justify-between itens-center px-5 h=12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+        {...props}
+        />
+            
     )
 }
--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,18 @@
-interface ButtonProps {
-    children: React.ReactNode;
-}
+import { ComponentProps, ReactNode } from "react";
+
+
+
+
+interface ButtonProps extends ComponentProps<"button"> { 
+    children: ReactNode;
+} 
 
 export function IconButton(props: ButtonProps) {
     return (
-        <button className="px-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300">
-            {props.children }
-        </button>
+        <button 
+        className="px-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+        {...props}
+        />
+           
     )
 }
