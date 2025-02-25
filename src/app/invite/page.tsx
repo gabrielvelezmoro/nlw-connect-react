a94a3f120fc909bd484bc4d2621128f7ab0554cc
Gabriel Velezmoro <gabrieuvelezmoro@gmail.com>
2025-02-24 22:07:42 -0300

    refactor: componentiza ranking

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -3,9 +3,8 @@ import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/Logo.svg'
-import medalCooper from '../../assets/medal-cooper.svg'
-import medalGold from '../../assets/medal-gold.svg'
-import medalSilver from '../../assets/medal-silver.svg'
+import { Ranking } from './ranking'
+
 
 export default function InvitePage() {
     return (
@@ -65,46 +64,8 @@ export default function InvitePage() {
                 </div>
             </div>
 
-            <div className='w-full max-w-[440px] space-y-5'>
-                <h2 className='text-gray-200 text-xl font-heading font-semibold'>
-                    Ranking de indicações
-                </h2>
-                <div className='space-y-4'>
-                    <div className=' relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
-                        <span className='text-sm text-gray-300 leading-none'>
-                            <span className='font-semibold'>1°</span> |
-                            Gabriel Velezmoro
-                        </span>
-                        <span className='font-heading text-2xl font-semibold leading-none'>
-                            1030
-                        </span>
+            <Ranking />
 
-                        <Image src={medalGold} alt='' className='absolute top-0 right-8' />
-                    </div>
-                    <div className=' relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
-                        <span className='text-sm text-gray-300 leading-none'>
-                            <span className='font-semibold'>2°</span> |
-                            Gabriel Velezmoro
-                        </span>
-                        <span className='font-heading text-2xl font-semibold leading-none'>
-                            1030
-                        </span>
-
-                        <Image src={medalSilver} alt='' className='absolute top-0 right-8' />
-                    </div>
-                    <div className=' relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3'>
-                        <span className='text-sm text-gray-300 leading-none'>
-                            <span className='font-semibold'>3°</span> |
-                            Gabriel Velezmoro
-                        </span>
-                        <span className='font-heading text-2xl font-semibold leading-none'>
-                            1030
-                        </span>
-
-                        <Image src={medalCooper} alt='' className='absolute top-0 right-8' />
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
